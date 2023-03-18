ba8c94c17439f01c20251f273d2fe37aeaea2e5b
Tolu Adegbehingbe <tolufive@gmail.com>
2023-03-18 12:11:48 -0700

    svg type definitions

--- metro.config.js
+++ metro.config.js
@@ -2,15 +2,14 @@ const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts, assetExts },
+    resolver: { sourceExts },
   } = await getDefaultConfig();
   return {
     transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
+      assetExts: ['svg', ...sourceExts],
     },
   };
 })();
