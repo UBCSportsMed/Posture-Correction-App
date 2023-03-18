95a2f87abd5c32ad83ec563ec4e243150e8cab3d
Gaurab Acharya <acharyaagaurab@gmail.com>
2023-03-18 11:09:15 -0700

    added svg config and updated expo

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SmallButton from './assets/svgs/small-button.svg';
+import SVG from 'react-native-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SmallButton width={200} height={200}/>
     </View>
   );
 }
