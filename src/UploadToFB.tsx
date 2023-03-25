65ff0908614e932dc24f2528b135ca09be303d03
Tony Li <dawner125@gmail.com>
2023-03-25 12:36:05 -0700

    Merged Main

---
+++ src/UploadToFB.tsx
@@ -0,0 +1,3 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Keyboard } from 'react-native'
+import React, { useState } from 'react'
+import { fireStore } from '../firebase-config/fb-config.js'
